420a395d3b65212057b4ae49cf46c694464539fd
JGeven <j.geven@student.fontys.nl>
2024-04-17 10:17:08 +0200

    Added security config

---
+++ src/main/java/babysimulator/datagenerator/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package babysimulator.datagenerator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
+    }
+}
