5fccde52a233eee163a8181d16f30b012ae799e0
JGeven <j.geven@student.fontys.nl>
2024-04-17 13:30:32 +0200

    Changed Security config for Docker

--- src/main/java/babysimulator/datagenerator/config/SecurityConfig.java
+++ src/main/java/babysimulator/datagenerator/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +17,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
+        return http
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().permitAll())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults())
+                .build();
     }
 }
