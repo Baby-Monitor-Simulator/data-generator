87741c94ec65f88f6d6ac5d806b251ea87e04d41
VKalyon <62647595+VKalyon@users.noreply.github.com>
2024-04-23 11:13:48 +0200

    retries

--- src/main/java/babysimulator/datagenerator/DataGeneratorApplication.java
+++ src/main/java/babysimulator/datagenerator/DataGeneratorApplication.java
@@ -12,8 +12,20 @@ public class DataGeneratorApplication {
 
 		MessageBus broker = new RabbitMQ();
 
-		// Connection retries still have to be added
-		broker.connect("localhost");
+		int maxTries = 5;
+		for (int count = 0; count <= maxTries; count++) {
+			try {
+				broker.connect("localhost");
+				break;
+			} catch (Exception e) {
+				if (count == maxTries) {
+					throw new Exception("Cannot connect to RabbitMQ. Shutting down.");
+				}
+				int sleepTime = 500 * (int) Math.pow(2, count);
+				Thread.sleep(sleepTime);
+				System.out.println("Retrying connection...");
+			}
+		}
 
 		// Once the API call has been added, the topic and message should be variables instead of hardcoded
 		broker.publishToTopic("1", "test message");
